fde3a34c6b74bb0753954ca135e4c4abbdf39ee0
Adriane Hanson <ahansonuga@gmail.com>
2022-01-14 16:35:57 -0500

    Add configuration file
    For AIT collection numbers. Easier to edit in a separate document and keeps confidential so can have a public repository.

---
+++ configuration_template.py
@@ -0,0 +1,8 @@
+# Values specific to different UGA accounts.
+
+# The Archive-It collection you download the most PDFs from. It may be left blank.
+ait_collection_default = ""
+
+# All possible Archive-It collections you download PDFs from.
+# It is used to verify there were no errors when providing the collection to the script.
+ait_collection_list = ["#####", "#####", "#####"]
